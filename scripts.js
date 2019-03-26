91b400d6939b0f1d49ed5f772c01f2800d70719e
Vincenzo Verduzco <vincenzoverduzco@Vincenzos-MacBook-Pro.local>
2019-03-26 18:00:24 -0500

    meh

--- scripts.js
+++ scripts.js
@@ -4,16 +4,7 @@ var modal = document.querySelector('.modal');
 
    trigger.addEventListener('click', toggleModal);
    closeButton.addEventListener('click', toggleModal);
-   window.addEventListener('click', windowOnClick);
 
-   function showModal() {
+   function toggleModal() {
        modal.classList.toggle('show-modal');
-   }
-
-
-
-  function windowOnClick(event) {
-      if (event.target === modal) {
-          showModal();
-       }
    }
