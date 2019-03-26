8210f8b8d5b7c249c8a14ebf49fd711b88df74ed
Vincenzo Verduzco <vincenzoverduzco@Vincenzos-MacBook-Pro.local>
2019-03-26 17:51:32 -0500

    no clue

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ var modal = document.querySelector(".modal");
    function toggleModal() {
        modal.classList.toggle("show-modal");
    }
+   trigger.addEventListener("click", toggleModal);
+   closeButton.addEventListener("click", toggleModal);
+   window.addEventListener("click", windowOnClick);
 
    function windowOnClick(event) {
        if (event.target === modal) {
@@ -12,6 +15,29 @@ var modal = document.querySelector(".modal");
        }
    }
 
-   trigger.addEventListener("click", toggleModal);
-   closeButton.addEventListener("click", toggleModal);
-   window.addEventListener("click", windowOnClick);
+
+   // Get the modal
+   var modal = document.getElementById('myModal');
+
+   // Get the button that opens the modal
+   var btn = document.getElementById("myBtn");
+
+   // Get the <span> element that closes the modal
+   var span = document.getElementsByClassName("close")[0];
+
+   // When the user clicks on the button, open the modal
+   btn.onclick = function() {
+     modal.style.display = "block";
+   }
+
+   // When the user clicks on <span> (x), close the modal
+   span.onclick = function() {
+     modal.style.display = "none";
+   }
+
+   // When the user clicks anywhere outside of the modal, close it
+   window.onclick = function(event) {
+     if (event.target == modal) {
+       modal.style.display = "none";
+     }
+   }
---
+++ scripts.js
@@ -0,0 +1,19 @@
+var modal = document.querySelector('.modal');
+   var trigger = document.querySelector('.trigger');
+   var closeButton = document.querySelector('.close-button');
+
+   trigger.addEventListener('click', toggleModal);
+   closeButton.addEventListener('click', toggleModal);
+   window.addEventListener('click', windowOnClick);
+
+   function showModal() {
+       modal.classList.toggle('show-modal');
+   }
+
+
+
+  function windowOnClick(event) {
+      if (event.target === modal) {
+          showModal();
+       }
+   }
