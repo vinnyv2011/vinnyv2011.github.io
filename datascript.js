5b8e0ddc16fb2ec6b9bdc39bb0e0f16e2d3275db
Vincenzo Verduzco <vincenzoverduzco@Vincenzos-MacBook-Pro.local>
2019-04-19 09:07:38 -0500

    zoom

--- datascript.js
+++ datascript.js
@@ -1,14 +1,15 @@
 
+
 var White = {
   x: [2000, 2004, 2008, 2012, 2016],
   y: [0, 6.7, 0.9, -3.4, 2.9],
   mode: 'lines+markers',
   marker: {
-    color: 'rgb(219, 64, 82)',
-    size: [5, 10, 15, 20, 25]
+    color: '#F59301',
+    size: [90, 80, 80, 70, 60]
   },
     line: {
-      color: 'rgb(219, 64, 82)',
+      color: '#F59301',
       width: 2
   },
   name: "White"
@@ -19,11 +20,11 @@ var Black = {
   y: [0, 8.5, 7.7, -1.7, -7.5],
   mode: 'lines+markers',
   marker: {
-    color: 'rgb(55, 128, 191)',
-    size: 12
+    color: '#ECF501',
+    size: [22.4, 22.2, 24.6, 26.4, 24.6]
   },
   line: {
-    color: 'rgb(55, 128, 191)',
+    color: '#ECF501',
     width: 2
   },
   name: "Black"
@@ -35,7 +36,7 @@ var Hispanic = {
   mode: 'lines+markers',
   marker: {
     color: 'rgb(128, 0, 128)',
-    size: 8
+    size: [10.8, 11.8, 14.6, 16.6, 18.2]
   },
   line: {
     color: 'rgb(128, 0, 128)',
@@ -50,7 +51,7 @@ var Other = {
   mode: 'lines+markers',
   marker: {
     color: 'rgb(16, 224, 19)',
-    size: 8
+    size: [4.8, 7.2, 7.6, 8.8, 10]
   },
   line: {
     color: 'rgb(16, 224, 19)',
@@ -62,14 +63,20 @@ var Other = {
 var data = [White, Black, Hispanic, Other];
 
 var layout = {
-  title: 'Presidential Election Voter Turnout',
-
+  title: {
+    text: 'Presidential Election Voter Turnout',
+  font: {
+    family: 'Courier New, monospace',
+    size: 30,
+    color: '#000'
+  }
+},
 yaxis: {
   title: {
-    text: '% Change in Turnout Rate',
+    text: '% Change in Turnout Rate Cycle to Cycle',
     font: {
       family: 'Courier New, monospace',
-      size: 18,
+      size: 14,
       color: '#000'
     }
   }
@@ -87,7 +94,7 @@ shapes: [
             x1: '2008',
             y1: 1,
             fillcolor: '#FF0000',
-            opacity: 0.3,
+            opacity: 0.2,
             line: {
                 width: 0
             }
@@ -103,7 +110,7 @@ shapes: [
             x1: '2016',
             y1: 1,
             fillcolor: '#0400F5',
-            opacity: 0.3,
+            opacity: 0.2,
             line: {
                 width: 0
             }
@@ -119,7 +126,7 @@ shapes: [
             x1: '2000',
             y1: 1,
             fillcolor: '#0400F5',
-            opacity: 0.3,
+            opacity: 0.2,
             line: {
                 width: 0
             }
@@ -135,7 +142,7 @@ shapes: [
             x1: '2017',
             y1: 1,
             fillcolor: '#FF0000',
-            opacity: 0.3,
+            opacity: 0.2,
             line: {
                 width: 0
             }
@@ -143,4 +150,4 @@ shapes: [
 ]
 };
 
-Plotly.newPlot('myDiv', data, layout);
+Plotly.newPlot('myDiv', data, layout, {scrollZoom: true});
